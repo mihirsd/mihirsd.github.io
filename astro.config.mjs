2ed56111b2f8b8d6a2f3646f62fdd3a798770db0
Mihir Desai <mihir@mihirdesai.net>
2025-08-10 10:42:14 +0530

    Setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://mihirdesai.net',
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
