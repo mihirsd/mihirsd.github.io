0690ed6cc59e5859fdf2d969866f1cb4a4f3603f
Mihir Desai <mihir@mihirdesai.net>
2025-08-10 10:45:02 +0530

    Setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://mihirdesai.net',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
