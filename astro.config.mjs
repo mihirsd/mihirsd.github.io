5f5725b2c4393f653b10a174e0814462e63760e2
Mihir Desai <mihir@mihirdesai.net>
2025-08-10 15:42:30 +0530

    chore: Update package.json to add build visualization and new dependencies
    - Added "build:visualize" script to enable visualization during build
    - Included "@astrojs/check" and "typescript" as dependencies
    - Added "rollup-plugin-visualizer" to devDependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   site: 'https://mihirdesai.net',
   integrations: [sitemap(), react()],
-
   vite: {
-    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+            three: ['three'],
+            r3fiber: ['@react-three/fiber'],
+            r3post: ['@react-three/postprocessing', 'postprocessing'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 500,
+    },
+    plugins: [
+      // @ts-expect-error
+      tailwindcss(),
+      ...(process.env.VISUALIZE === 'true' ? [visualizer({ open: true })] : []),
+    ],
   },
 });
